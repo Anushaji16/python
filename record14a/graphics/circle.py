e5fdb7a60e281c3fb481f4e5172f2714412acd5b
Anushaji16 <149172322+Anushaji16@users.noreply.github.com>
2025-10-29 10:38:53 +0530

    completed

---
+++ record14a/graphics/circle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+def circlearea(r):
+    return math.pi*r*r
+def circleperi(r):
+     return 2*math.pi*r
+
+
+# In[3]:
+
+
+get_ipython().system('jupyter nbconvert --to python circle.ipynb')
+
+
+# In[ ]:
+
+
+
+
---
+++ record14a/graphics/rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def rectarea (l,b):
+    print("Area of rectangle:",l*b)
+def rectperi (l,b):
+    print("perimeter of rectangle:",2*(l+b))
+    
+    
+
+
+# In[2]:
+
+
+get_ipython().system('jupyter nbconvert --to python rectangle.ipynb')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
